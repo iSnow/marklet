f4cbe9daaf54802dff6936638c8a045e69263e7a
i.kadochnikov@mail.msk <i.kadochnikov@corp.mail.ru>
2021-04-07 14:02:07 +0500

    write file method

--- src/main/java/io/github/atlascommunity/marklet/builders/ClassPage.java
+++ src/main/java/io/github/atlascommunity/marklet/builders/ClassPage.java
@@ -1,6 +1,12 @@
 package io.github.atlascommunity.marklet.builders;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.sun.javadoc.ClassDoc;
 
@@ -18,17 +24,37 @@ public class ClassPage implements DocumentPage {
   @Override
   public void build() throws IOException {
 
-    StringBuilder pageTitle =
+    StringBuilder classPage =
         new StringBuilder()
             .append(new Heading(new ClassTitle(classDoc).generate(), 1))
             .append("\n");
 
-    pageTitle.append(new ClassSummary(classDoc).generate());
+    classPage.append(new ClassSummary(classDoc).generate());
 
     String constructorsInfo = new ClassConstructorsInfo(classDoc, options).generate();
-    if (!constructorsInfo.isEmpty()) pageTitle.append(constructorsInfo);
+    if (!constructorsInfo.isEmpty()) classPage.append(constructorsInfo);
 
     String fieldsInfo = new ClassFieldsInfo(classDoc).generate();
-    if (!fieldsInfo.isEmpty()) pageTitle.append(fieldsInfo);
+    if (!fieldsInfo.isEmpty()) classPage.append(fieldsInfo);
+
+    String methodsInfo = new ClassMethodsInfo(classDoc).generate();
+    if (!methodsInfo.isEmpty()) classPage.append(methodsInfo);
+
+    writeFile(classPage);
+  }
+
+  private void writeFile(StringBuilder classPage) throws IOException {
+
+    String outputDirectory = options.getOutputDirectory();
+    String packageLayout = classDoc.containingPackage().name().replace('.', '/');
+    Path filename = Paths.get(classDoc.simpleTypeName() + "." + options.getFileEnding());
+
+    FileOutputStream outputStream =
+        new FileOutputStream(
+            Paths.get(outputDirectory, packageLayout).resolve(filename).toString());
+
+    try (Writer readmeFile = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+      readmeFile.write(classPage.toString());
+    }
   }
 }
